df989f8d27e2956c85ba8d41955649b6f583c7f9
phithong2005 <phithong2005@yahoo.com>
2014-11-27 11:53:57 +0700

    Revert "init"
    This reverts commit ba7d57a36d36b687a567a6295ef682d15b9d2809.

--- Cooing/Cooing/Adapter.cpp
+++
@@ -1,7 +0,0 @@
-#include "Global.h"
-
-void Adapter::ReceiveMsgHandle(string msg, string from)
-{
-	// Global::mainForm->receiveMessage(msg, from);
-	emit ReceiveMsgSignal(QString(msg.c_str()), QString(from.c_str()));
-}
